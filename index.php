6e50f24814bad604c21d821b8a533eae46bc431e
Arshxll <70693770+Arshxll@users.noreply.github.com>
2020-09-05 17:11:32 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="em">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width-device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-compatible" content="ie-edge">
+	<title> web </title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+ 
+	<header>
+			<p1> ARTIST-TA! </p1>
+	 <div class ="circle"> </div>
+	<div class="color-red"> 
+	<div class="color-green"> 
+	<div class ="color-yellow">
+	<div class="holder"> </div>
+		</h1> </h1>
+		<p>  </p>
+	</header>
+	<nav>
+		
+		<ul>
+			<li><a class="active" href="#">HOME</a></li>
+			<li><a href="#">ABOUT</a></li>
+			<li><a href="#">CONTACT</a></li>
+			<li><a href="#">FEEDBACK</a></li>
+			<li><a href="#">GALLERY</a></li>
+	</nav>
+	
+	<div class="wrapper">
+		<h1>   </h1>
+		<img class="img-supply" src="img/supply.jpg" alt="">
+	</div>
+	
+	<main>
+		<article> 
+			<h2>Lorem ipsum </h2>
+			<p> Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum Lorem ipsum Lorem ipsumLorem ipsum </p>
+			<p> Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum </p>
+		</article>
+		<article>
+			<h2> Lorem ipsum </h2>
+			<p> Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum Lorem ipsum Lorem ipsum </p>
+			</p> Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum Lorem ipsumLorem ipsum </p>
+		</article>
+		<article> 
+			<h2> Lorem ipsum </h2>
+			<p> Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </p>
+			<p> Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum </p>
+			<p> Lorem ipsum</p>
+	</main>
+	<footer>
+		<p> copyright &copy;   </p>
+		<p> FOOTER </p>
+		<p>  Lorem ipsum </p>
+	</footer>
+	
+</body>
+</html>
